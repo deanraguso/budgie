1a8aab4de8ddfa4acc5f2ce518858bb44a3375d7
Dean Raguso <deanraguso@gmail.com>
2022-05-31 22:45:34 +1000

    feat: configured amd styled dark-mode toggle

--- src/ColourModeToggle.tsx
+++ src/ColourModeToggle.tsx
@@ -1,13 +1,28 @@
-import { Button, useColorMode } from "@chakra-ui/react";
+import { Button, Flex, useColorMode } from "@chakra-ui/react";
+import { MoonStars, Sun } from "phosphor-react";
 import React from "react";
 
 const ColourModeToggle: React.FC = () => {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
-    <Button size="sm" colorScheme="blue" onClick={toggleColorMode}>
-      Toggle Mode
-    </Button>
+    <Flex pos="absolute" w="100%" justifyContent="flex-end" pt={2}>
+      <Button
+        aria-label="Toggle Color Mode"
+        onClick={toggleColorMode}
+        _focus={{ boxShadow: "none", bg: "" }}
+        _hover={{ boxShadow: "none", bg: "" }}
+        _active={{ boxShadow: "none", bg: "" }}
+        w="fit-content"
+        bg=""
+      >
+        {colorMode === "light" ? (
+          <MoonStars fill="solid" size={28} />
+        ) : (
+          <Sun fill="solid" size={28} />
+        )}
+      </Button>
+    </Flex>
   );
 };
 
